27de1d2c8f52d2af04275e976854afa733782a84
ejguzman3988 <eriberto176@gmail.com>
2020-07-05 23:35:15 -0400

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,23 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artist = []
+    self.songs.each do |song|
+      artist << song.artist_id
+    end
+
+    artist.uniq.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artist_name = []
+    self.songs.each{|song| artist_name << Artist.find_by(id: song.artist_id).name}
+    artist_name
   end
 end
